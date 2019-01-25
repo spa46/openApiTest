67032f96b3a30591171dcd08d86d296bb185a8fc
Sean Park <psckyky@hotmail.com>
2019-01-25 04:27:37 +0000

    Porting chartjs Succeded
    Checked line chart only so far.

--- projectS/settings.py
+++ projectS/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'analyzer.apps.AnalyzerConfig',
+    'scharts.apps.SChartsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chartjs'
 ]
 
 MIDDLEWARE = [
